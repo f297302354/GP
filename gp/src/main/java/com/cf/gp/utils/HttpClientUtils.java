8764250c733ec4c2715fbc1244855c788fd68cd8
f297302354 <297302354@qq.com>
2018-01-04 13:30:20 +0800

    1

--- gp/src/main/java/com/cf/gp/utils/HttpClientUtils.java
+++ gp/src/main/java/com/cf/gp/utils/HttpClientUtils.java
@@ -2,4 +2,11 @@ package com.cf.gp.utils;
 
 public class HttpClientUtils {
 
+	/**
+	 * 知乎：https://www.zhihu.com/question/22145919
+	 * 新浪：http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=35&num=100&sort=symbol&asc=1&node=hs_a&symbol=&_s_r_a=init
+	 * 搜狐：http://q.stock.sohu.com/hisHq?code=zs_000001&start=20171228&end=20171229&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp&r=0.8391495715053367&0.9677250558488026
+	 */
+	
+	
 }
