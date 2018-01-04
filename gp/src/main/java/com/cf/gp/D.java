c6f81d1de574e59cddb0e0d7f220862b84e4ea21
f297302354 <297302354@qq.com>
2018-01-04 09:46:13 +0800

    1

---
+++ gp/src/main/java/com/cf/gp/D.java
@@ -0,0 +1,9 @@
+package com.cf.gp;
+
+public class D {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ gp/src/test/java/com/cf/gp/DD.java
@@ -0,0 +1,9 @@
+package com.cf.gp;
+
+public class DD {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
