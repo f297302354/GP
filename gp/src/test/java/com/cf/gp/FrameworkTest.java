ba4fe98c942df41d785eb19ea840e0e93fdaac69
f297302354 <297302354@qq.com>
2018-01-08 10:53:21 +0800

    1

--- gp/src/test/java/com/cf/gp/FrameworkTest.java
+++ gp/src/test/java/com/cf/gp/FrameworkTest.java
@@ -10,7 +10,7 @@ import com.cf.gp.service.TestInterface;
 public class FrameworkTest {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext(new String[]{"spring/spring.xml","mybatis/mybatis-config.xml"});
+		ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext(new String[]{"spring/spring.xml","mybatis/spring-mybatis.xml"});
 		TestInterface bean = (TestInterface)app.getBean("testService");
 		List<TestModel> queryModel = bean.queryModel();
 		System.out.println(queryModel.size());
